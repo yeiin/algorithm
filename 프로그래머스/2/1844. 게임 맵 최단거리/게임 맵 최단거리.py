425e9b95f2c64fbb171164a0b51959313c318717
yeiin <88276674+yeiin@users.noreply.github.com>
2024-04-09 23:32:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.81 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,24 @@
 from collections import deque
   
 def solution(maps):
-    stack = deque()
+    queue = deque([(0,0)])
     if(maps[0][0] == 0):
         return -1
     
-    stack.append((0,0))
-
+    
     width = len(maps[0])-1
     height = len(maps) -1
     vec = [(-1, 0), (0, -1), (1, 0), (0,1)]
 
-    while stack:
+    while queue:
     
-        item = stack.popleft()
+        item = queue.popleft()
 
         for k in vec:
             x = item[0] + k[0]
             y = item[1] + k[1]
             if((0 <= x <= height) and (0 <=y<= width) and (maps[x][y] == 1) and ((x,y) != (0,0))):
-                stack.append((x,y))
+                queue.append((x,y))
                 maps[x][y] = maps[item[0]][item[1]] + 1
         
 
