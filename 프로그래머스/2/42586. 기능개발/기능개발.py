bf6113371e7c27a6c4ae2de6047ae8a2a6f54b0a
yeiin <88276674+yeiin@users.noreply.github.com>
2024-04-12 03:11:43 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,28 +1,33 @@
-def solution(progresses, speeds):
-    answer = []
-    queue = []
-    
-    for idx, item in enumerate(progresses):
-        count = 0
-        while (item < 100):
-            count += 1
-            item += speeds[idx]
-        queue.append(count)
-    
-    count = 0
-    temp = queue[0]
-    
-    for i, j in enumerate(queue):
-        if(temp >= j):
-            count += 1
+from collections import deque
 
-            if(i == len(queue)-1):
-                answer.append(count)
-        else:
-            answer.append(count)
-            count = 1
-            if(i == len(queue)-1):
-                answer.append(count)
-            else:
-                temp = j
-    return answer
+def solution(progresses, speeds):
+  queue = deque()
+  answer = []
+
+  for idx,i in enumerate(progresses):
+    count = 0
+    while i < 100:
+      i+=speeds[idx]
+      count+=1
+    queue.append(count)
+
+  item = queue[0]
+  count = 0
+
+  while queue:
+    temp = queue.popleft()
+
+    if(item >= temp):
+      count += 1
+      if not queue:
+        answer.append(count)
+
+    else:
+      answer.append(count)
+      item = temp
+      count = 1
+
+      if not queue:
+        answer.append(count)
+
+  return answer
