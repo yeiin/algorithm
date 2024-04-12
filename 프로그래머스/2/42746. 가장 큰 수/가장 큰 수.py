137ac0de896b50ccf0c58332683ada7c5d740cc0
yeiin <88276674+yeiin@users.noreply.github.com>
2024-04-12 15:43:50 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,33 +1,9 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-
-    left = []
-    right = []
-    equal = []
-    for i in range(len(arr)):
-        if(arr[i] == pivot):
-            equal.append(pivot)
-        else:
-
-            temp1 = arr[i] * (10 ** len(str(pivot))) + pivot
-            temp2 = pivot * (10 ** len(str(arr[i]))) + arr[i]
-    
-            if temp1 >= temp2:
-                left.append(arr[i])
-            elif temp1 < temp2:
-                right.append(arr[i])
-    return quicksort(left) + equal + quicksort(right)
-
-
 def solution(numbers):
-    answer = ''
-    if(numbers.count(0)==len(numbers)):
-        answer = "0"
-    else:
-        sortedNum = quicksort(numbers)
-    
-        answer = "".join(map(str, sortedNum))
+  num_arr = list(map(str, numbers))
+  num_arr.sort(key=lambda x:x*3, reverse = True)
+  
 
-    return answer
+  answer = "".join(num_arr)
+  if(answer[0]=="0"):
+    return "0"
+  return answer
