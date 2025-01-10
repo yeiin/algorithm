dff914f2831a7bace8f37b6a7a03bdcd4e34d7de
yeiin <88276674+yeiin@users.noreply.github.com>
2025-01-11 00:24:33 +0900

    [Silver I] Title: 스타트와 링크, Time: 336 ms, Memory: 18496 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    static int N;
+    static int[][] abilityArr;
+    static boolean[] visitied;
+    static int minium;
+
+
+    public static void dfs(int node, int depth){
+
+        if(depth==N/2){
+            int start = 0;
+            int link = 0;
+
+            for(int i=0; i<N-1; i++){
+                for(int j=i+1; j<N; j++){
+                    if(visitied[i] && visitied[j]){
+                        start += abilityArr[i][j]+abilityArr[j][i];
+                    }else if(!visitied[i] && !visitied[j]){
+                        link += abilityArr[i][j]+abilityArr[j][i];
+                    }
+                }
+            }
+
+            int cal = Math.abs(start - link);
+
+            minium = Math.min(minium, cal);
+        }else{
+            for(int i=node; i<N; i++){
+                if(!visitied[i]){
+                    visitied[i] = true;
+                    dfs(i+1, depth+1);
+                    visitied[i] = false;
+                }
+            }
+        }
+
+    }
+    
+
+    public static void main(String[] args)throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        abilityArr = new int[N][N];
+        visitied = new boolean[N];
+        minium = 2000000;
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                abilityArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 0);
+
+        System.out.println(minium);
+
+
+    }
+}
