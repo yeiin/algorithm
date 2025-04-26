43c8db5872c66c5ed602412ef04dbc3f63383fb8
yeiin <88276674+yeiin@users.noreply.github.com>
2025-04-26 16:33:01 +0900

    Update ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,50 +1,39 @@
 class Solution {
-    
-    int answer = 0;
-    int max = 0;
-    
-    public void check(int[][] coor){
-        int[][] timeArr = new int[max+1][max+1];
-        for(int j=0; j<coor.length; j++){
-            if(coor[j][0]<0){
-                    continue;
-                }
-                System.out.println("j: "+ coor[j][0]+", "+coor[j][1]);
-                if(timeArr[coor[j][0]][coor[j][1]]==1){
-                    answer++;
-                }
-                timeArr[coor[j][0]][coor[j][1]]++;
-                
-            }
-                 
-            System.out.println(answer);
-            System.out.println();
-    }
-    
+
     public int solution(int[][] points, int[][] routes) {
-       
+        int answer = 0;
+        int max = 0;
         int completeRobots = 0;
+        
         int[][] coor = new int[routes.length][2];
         int[] nextGoal = new int[routes.length];
         int[] nextIndex = new int[routes.length];
-        
-        for(int i=0; i<routes.length; i++){
-            nextGoal[i] = routes[i][1]-1;
-            nextIndex[i] = 1;
-            coor[i] = points[routes[i][0]-1].clone();
-        }
         
         for(int i=0; i<points.length; i++){
             max = Math.max(max, Math.max(points[i][0],points[i][1]));
         }
         
+        int[][] timeArr = new int[max+1][max+1];
         
-        check(coor);
+        for(int i=0; i<routes.length; i++){
+            nextGoal[i] = routes[i][1]-1;
+            nextIndex[i] = 1;
+            coor[i] = points[routes[i][0]-1].clone();
+            
+            if(timeArr[coor[i][0]][coor[i][1]]==1){
+                    answer++;
+            }
+            
+            timeArr[coor[i][0]][coor[i][1]]++;
+        }
         
+        
+                  
         while(completeRobots < routes.length){
-            int[][] timeArr = new int[max+1][max+1];
+            timeArr = new int[max+1][max+1];
             
             for(int i=0; i<coor.length; i++){  
+                
                 if(nextIndex[i] == -1){
                     continue;
                 }
@@ -52,31 +41,21 @@ class Solution {
                 int[] nextPoint = points[nextGoal[i]].clone();
                                 
                 if(coor[i][0] != nextPoint[0]){
-                    if(nextPoint[0] > coor[i][0]){
-                        coor[i][0]+=1;
-                    }else{
-                        coor[i][0]-=1;
-                    }
+                    coor[i][0] += Integer.compare(nextPoint[0], coor[i][0]);
                 }else if(coor[i][1] != nextPoint[1]){
-                    if(nextPoint[1] > coor[i][1]){
-                        coor[i][1]+=1;
-                    }else{
-                        coor[i][1]-=1;
-                    }
+                    coor[i][1] += Integer.compare(nextPoint[1], coor[i][1]);
                 }
                 
                 if(timeArr[coor[i][0]][coor[i][1]]==1){
                     answer++;
                 }
+                
                 timeArr[coor[i][0]][coor[i][1]]++;
                 
                 if(coor[i][0]==nextPoint[0] && coor[i][1] == nextPoint[1]){
-                    if(nextIndex[i] == routes[0].length-1){
+                    if(nextIndex[i] == routes[i].length - 1){
                         completeRobots++;
                         nextIndex[i] = -1;
-                        coor[i][0] = -1;
-                        coor[i][1] = -1;
-                        
                     }else{
                         nextIndex[i] += 1;
                         nextGoal[i] = routes[i][nextIndex[i]]-1;
