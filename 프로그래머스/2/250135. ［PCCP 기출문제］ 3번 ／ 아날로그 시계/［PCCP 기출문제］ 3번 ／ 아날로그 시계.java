26a65f6b1f891af6d7b6827979966405f263f798
yeiin <88276674+yeiin@users.noreply.github.com>
2025-07-02 15:37:14 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 26.36 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,56 +1,48 @@
+import java.util.*;
+
 class Solution {
+ 
+    public double[] next(double second){
+        double[] angle = new double[3];
+                
+        angle[0] = (second / 120) % 360;
+        angle[1] = (second / 10) % 360;
+        angle[2] = (second * 6) % 360;
+        
+        return angle;
+    }
+     
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
         int answer = 0;
+
+        double time1 = h1*60*60 + m1*60 + s1;
+        double time2 = h2*60*60 + m2*60 + s2;
         
-        double startTime = h1 * 3600 + m1 * 60 + s1;
-        double endTime = h2 * 3600 + m2 * 60 + s2;
+        if(time1==0 || time1 == 12*3600) answer++;
         
-        
-        if(startTime == 0 || startTime == 12*3600){
-            answer += 1;
-        }
-        
-        while (startTime < endTime){
+        while(time1<time2){            
+                  
+            double[] angle1 = next(time1); 
+            double[] angle2 = next(time1+1);
             
-            double currH = startTime / 120 % 360;
-            double currM = startTime / 10 % 360;
-            double currS = startTime * 6 % 360;
-            
-            
-            double nextH = (startTime + 1) / 120 % 360;
-            double nextM = (startTime + 1) / 10 % 360;
-            double nextS = (startTime + 1) * 6 % 360;
-            
-            if(nextH == 0){
-                nextH = 360;
+             
+            if(angle2[0] == 0) angle2[0] = 360;
+            if(angle2[1] == 0) angle2[1] = 360;
+            if(angle2[2] == 0) angle2[2] = 360;
+
+            if(angle1[0] > angle1[2] && angle2[0] <= angle2[2]){
+                answer ++;
             }
             
-            if(nextM == 0){
-                nextM = 360;
+            if(angle1[1]>angle1[2] && angle2[1]<=angle2[2]){
+                answer++;
             }
             
-            if(nextS == 0){
-                nextS = 360;
+            if(angle2[0] == angle2[1] && angle2[1]==angle2[2]){
+                answer--;
             }
             
-            
-            if(currS < currH && nextS >= nextH){
-              
-                answer += 1;
-            }
-            
-            if(currS < currM && nextS >= nextM){
-    
-                answer += 1;
-            }
-            
-            if(nextS == nextM && nextH == nextM){
-                answer -= 1;
-            }
-            
-            
-            startTime += 1;
-            
+            time1++;
         }
         
         return answer;
