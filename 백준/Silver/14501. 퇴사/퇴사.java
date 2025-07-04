1090df4bd3b4e0b20f2758d3b49a36dbdd055378
yeiin <88276674+yeiin@users.noreply.github.com>
2025-07-04 11:11:34 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,28 +1,29 @@
 import java.util.*;
 import java.io.*;
-
 public class Main{
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
-        int[] time = new int[number];
-        int[] price = new int[number];
-        int[] dp = new int[number+1];
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<number; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
-            price[i] = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+        int[] dp = new int[n+1];
+        StringTokenizer st;
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0; i<number; i++){
-           
-            if(i+time[i]<=number){
-                dp[i+time[i]] = Math.max(dp[i+time[i]], dp[i]+price[i]);
+        for(int i=0; i<n; i++){
+            if(i+arr[i][0]<=n){
+                dp[i+arr[i][0]] = Math.max(dp[i+arr[i][0]], dp[i]+arr[i][1]);
             }
-            dp[i+1] = Math.max(dp[i], dp[i+1]);
-            
+
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
         }
-        System.out.println(dp[number]);
+
+        System.out.println(dp[n]);
     }
 }
