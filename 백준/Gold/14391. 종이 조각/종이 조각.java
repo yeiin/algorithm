e0ff9ecc0aa975a7585e8b0bbe8e4c0752a798f4
yeiin <88276674+yeiin@users.noreply.github.com>
2025-07-07 13:00:03 +0900

    [Gold III] Title: 종이 조각, Time: 164 ms, Memory: 18660 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -1,60 +1,98 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main{
+
+    static int n;
+    static int m;
+
+    static int[][] arr;
+    static boolean[][] horizontal;
+
+    static int answer = 0;
+
+
+    public static void dfs(int x, int y){
+        int sum = 0;
+
+        if(x==n && y==m){
+
+            for(int i=0; i<n; i++){
+                int temp = 0;
+                for(int j=0; j<m; j++){
+                    if(horizontal[i][j]){
+                        temp*=10;
+                        temp+=arr[i][j];
+                    }else{
+                        sum+=temp;
+                        temp = 0;
+                    }
+                }
+                sum+=temp;
+            }
+
+
+            for(int i=0; i<m; i++){
+                int temp = 0;
+                for(int j=0; j<n; j++){
+                    if(!horizontal[j][i]){
+                        temp*=10;
+                        temp+=arr[j][i];
+                    }else{
+                        sum+=temp;
+                        temp = 0;
+                    }
+                }
+               sum+=temp;
+            }
+
+            
+            answer = Math.max(answer, sum);           
+            return;
+        }
+        
+        int nextX;
+        int nextY;
+
+        if(y+1<m){
+            nextX = x;
+            nextY = y+1;
+        }else if(x+1<n){
+            nextX = x+1;
+            nextY = 0;
+        }else{
+            nextX=n;
+            nextY=m;
+        }
+       
+       horizontal[x][y] = true;
+       dfs(nextX, nextY);
+       horizontal[x][y] = false;
+       dfs(nextX, nextY);
+       
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-
-        int max = 0;
+        arr = new int[n][m];
+        horizontal = new boolean[n][m];
 
         for(int i=0; i<n; i++){
-            String input = br.readLine();
+            String[] strings = br.readLine().split("");
             for(int j=0; j<m; j++){
-                arr[i][j] = input.charAt(j)-'0';
+                arr[i][j] = Integer.parseInt(strings[j]);
             }
         }
 
-        for(int b=0; b<(1<<(n*m)); b++){
-            int sum = 0;
+        dfs(0, 0);
 
-            for(int i=0; i<n; i++){
-                int num = 0;
-                for(int j=0; j<m; j++){
-                    int idx = i * m + j;
-                    if ((b & (1 << idx)) == 0) {
-                        num = num * 10 + arr[i][j];
-                    } else {
-                        sum += num;
-                        num = 0;
-                    }
-                }
-                sum += num;
-            }
-
-            for (int j = 0; j < m; j++) {
-                int num = 0;
-                for (int i = 0; i < n; i++) {
-                    int idx = i * m + j;
-                    if ((b & (1 << idx)) != 0) {
-                        num = num * 10 + arr[i][j];
-                    } else {
-                        sum += num;
-                        num = 0;
-                    }
-                }
-                sum += num;
-            }
-
-            max = Math.max(max, sum);
-        }
-        
-        System.out.println(max);
+        System.out.println(answer);
 
     }
 }
