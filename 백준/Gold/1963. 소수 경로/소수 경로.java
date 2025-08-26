872b170a21f1b6dd6f317a1965eeb1161cc4b8bb
yeiin <88276674+yeiin@users.noreply.github.com>
2025-08-26 22:40:01 +0900

    [Gold IV] Title: 소수 경로, Time: 164 ms, Memory: 23008 KB -BaekjoonHub

---
+++ 백준/Gold/1963. 소수 경로/소수 경로.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+
+        int max = 0;
+        List<int[]> lists = new ArrayList<>();
+
+        for(int t=0; t<T; t++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            max = Math.max(max, Math.max(a, b));
+            lists.add(new int[]{a, b});
+        }
+
+        boolean[] nDecimals = new boolean[10001];
+        nDecimals[0] = true;
+        nDecimals[1] = true;
+
+        for(int i=2; i<=Math.sqrt(10000); i++){
+            if(nDecimals[i]) continue;
+
+            for(int j = i*i; j <= nDecimals.length-1; j = j+i) {
+				nDecimals[j] = true;
+			}
+        }
+
+        for(int[] l: lists){
+
+            boolean[] visited = new boolean[10001];
+            Queue<int[]> q = new LinkedList<>();
+            q.add(new int[]{l[0], 0});
+
+            int min = Integer.MAX_VALUE;
+
+            while(!q.isEmpty()){
+                int[] curr = q.poll();
+
+                if(curr[0]==l[1]){
+                    min = Math.min(min, curr[1]);
+                    continue;
+                }
+
+                if(curr[1]>=min) continue;
+
+                for(int i=0; i<4; i++){
+                    char[] digits = String.valueOf(curr[0]).toCharArray();
+                    for(char d='0'; d<='9'; d++){
+                        if (digits[i] == d) continue;
+                        if (i == 0 && d == '0') continue;
+
+                        char old = digits[i];
+                        digits[i] = d;
+                        int next = Integer.parseInt(new String(digits));
+                        digits[i] = old;
+
+                        if (!nDecimals[next] && !visited[next]) {
+                            visited[next] = true;
+                            q.add(new int[]{next, curr[1] + 1});
+                        }
+                    }
+
+                    
+                }
+            }
+
+            if(min==Integer.MAX_VALUE) System.out.println("Impossible");
+            else System.out.println(min);
+        }
+        
+    }
+}
