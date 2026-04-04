a65aeac873006ee019f2606846dc65c55b669df8
yeiin <88276674+yeiin@users.noreply.github.com>
2026-04-04 14:43:04 +0900

    [level 3] Title: 합승 택시 요금, Time: 87.66 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,94 +1,84 @@
 import java.util.*;
 
-class Solution {    
-    
-    public int solution(int n, int s, int a, int b, int[][] fares) {    
-        HashMap<Integer, List<int[]>> faresMap = new HashMap();
-    
-        int[] oDist;
-        int[] aDist;
-        int[] bDist;
-        int[] parents;
-
-        int minRoute = Integer.MAX_VALUE;
+class Solution {
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = Integer.MAX_VALUE;
         
-        for(int[] fare: fares){
-            
-            if(!faresMap.containsKey(fare[0])){
-                faresMap.put(fare[0], new ArrayList());
+        HashMap<Integer, List<int[]>> map = new HashMap<>();
+        
+        for(int[] f: fares){
+            if(!map.containsKey(f[0])){
+                map.put(f[0], new LinkedList());
             }
             
-            if(!faresMap.containsKey(fare[1])){
-                faresMap.put(fare[1], new ArrayList());
+            if(!map.containsKey(f[1])){
+                map.put(f[1], new LinkedList());
             }
             
-            faresMap.get(fare[0]).add(new int[]{fare[1], fare[2]});
-            faresMap.get(fare[1]).add(new int[]{fare[0], fare[2]});
+            map.get(f[0]).add(new int[]{f[1], f[2]});
+            map.get(f[1]).add(new int[]{f[0], f[2]});
         }
-            
-        oDist = new int[n+1];
-        aDist = new int[n+1];
-        bDist = new int[n+1];
     
-        Arrays.fill(oDist, Integer.MAX_VALUE);
+        
+        int[] sDist = new int[n+1];
+        int[] aDist = new int[n+1];
+        int[] bDist = new int[n+1];
+        
+        Arrays.fill(sDist, Integer.MAX_VALUE);
         Arrays.fill(aDist, Integer.MAX_VALUE);
         Arrays.fill(bDist, Integer.MAX_VALUE);
         
-        oDist[s] = 0;
-        aDist[a] = 0;
-        bDist[b] = 0;
-    
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1]-o2[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)->{return o1[1]-o2[1];});
         pq.add(new int[]{s, 0});
+        sDist[s]=0;
         
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
             
-            faresMap.get(curr[0]).sort((o1, o2) -> o1[1] - o2[1]);
-            
-            for(int[] f: faresMap.get(curr[0])){
-                
-                if(oDist[f[0]]> oDist[curr[0]] + f[1]){
-                    oDist[f[0]] = oDist[curr[0]] + f[1];
-                    pq.add(f);
+            for(int[] t: map.get(curr[0])){
+                if(sDist[t[0]]>(sDist[curr[0]]+t[1])){
+                    sDist[t[0]] = sDist[curr[0]]+t[1];
+                    pq.add(t);
                 }
+                
             }
         }
         
         pq.add(new int[]{a, 0});
+        aDist[a]=0;
         
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
             
-            for(int[] f: faresMap.get(curr[0])){
-                if(aDist[f[0]]> aDist[curr[0]] + f[1]){
-                    aDist[f[0]] = aDist[curr[0]] + f[1];
-                    pq.add(f);
+            for(int[] t: map.get(curr[0])){
+                if(aDist[t[0]]>(aDist[curr[0]]+t[1])){
+                    aDist[t[0]] = aDist[curr[0]]+t[1];
+                    pq.add(t);
                 }
+                
             }
         }
-          
+        
         pq.add(new int[]{b, 0});
+        bDist[b]=0;
         
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
-                    
-            for(int[] f: faresMap.get(curr[0])){
-                if(bDist[f[0]]> bDist[curr[0]] + f[1]){
-                    bDist[f[0]] = bDist[curr[0]] + f[1];
-                    pq.add(f);
-                }
-            }
-        }
-        
-        for(int i=1; i<n+1; i++){
             
-            if(oDist[i]!=Integer.MAX_VALUE && aDist[i]!=Integer.MAX_VALUE && bDist[i]!=Integer.MAX_VALUE){
-                minRoute = Math.min(minRoute, oDist[i]+aDist[i]+bDist[i]);
+            for(int[] t: map.get(curr[0])){
+                if(bDist[t[0]]>(bDist[curr[0]]+t[1])){
+                    bDist[t[0]] = bDist[curr[0]]+t[1];
+                    pq.add(t);
+                }
+                
             }
         }
         
-        return minRoute;
+        for(int i=1; i<=n; i++){
+            if(sDist[i]==Integer.MAX_VALUE||aDist[i]==Integer.MAX_VALUE||bDist[i]==Integer.MAX_VALUE) continue;
+            answer = Math.min(answer, sDist[i]+aDist[i]+bDist[i]);
+        }
+        
+        return answer;
     }
 }
