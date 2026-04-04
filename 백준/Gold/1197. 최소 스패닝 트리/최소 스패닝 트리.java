1fa2286c8e179123a04788347f77168b5fcc3800
yeiin <88276674+yeiin@users.noreply.github.com>
2026-04-04 15:02:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 676 ms, Memory: 48812 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,73 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+class Main{
 
+    static int v;
+    static int[] parents;
+
+    public static void setParents(int[] curr){
+        int pa = parents[curr[0]];
+        int pb = parents[curr[1]];
+
+        for(int i=1; i<=v; i++){
+            if(parents[i]==pa || parents[i]==pb){
+                parents[i] = pa;
+            }
+        }
+    }
     public static void main(String[] args) throws IOException{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());        
-        
-        List<int[]> graph = new ArrayList<>();
+        v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
 
+        long answer = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)-> o1[2]-o2[2]);
+        
         for(int i=0; i<e; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            graph.add(new int[]{a, b, w});
-            
-        }
-
-        graph.sort((o1, o2)-> o1[2] - o2[2]);
-        
-        long answer = 0;
-        HashSet<Integer> visited = new HashSet<>();
-        int[] parents = new int[v+1];
-
-        for(int i=1; i<v+1; i++){
-            parents[i] = i;
-         }
-
-        for(int[] edge: graph){
-            
-            if(!visited.contains(edge[0]) || !visited.contains(edge[1])){
-                answer += edge[2];
-                visited.add(edge[0]);
-                visited.add(edge[1]);
-
-                int pa = parents[edge[0]];
-                int pb = parents[edge[1]];
-                int p = Math.min(pa,pb);
-
-                for(int i=1; i<v+1; i++){
-                    if(parents[i] == pa || parents[i] == pb){
-                        parents[i] = p;
-                    }
-                }
-
-            }else if(parents[edge[0]] != parents[edge[1]]){
-
-                answer += edge[2];
-
-                int pa = parents[edge[0]];
-                int pb = parents[edge[1]];
-                int p = Math.min(pa,pb);
-
-                for(int i=1; i<v+1; i++){
-                    if(parents[i] == pa || parents[i] == pb){
-                        parents[i] = p;
-                    }
-                }
-
+            if(a>b){
+                int temp = a;
+                a = b;
+                b = temp;
             }
 
+            pq.add(new int[]{a, b, c});
         }
 
+        parents = new int[v+1];
+        boolean[] visited = new boolean[v+1];
+
+        for(int i=1; i<=v; i++){
+            parents[i] = i;
+        }
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+
+            if(!visited[curr[0]] || !visited[curr[1]]){
+                visited[curr[0]] = true;
+                visited[curr[1]] = true;
+                answer += curr[2];
+                setParents(curr);
+            }else if(parents[curr[0]]!= parents[curr[1]]){
+                answer += curr[2];
+                setParents(curr);
+            }
+        }
         System.out.println(answer);
+
     }
+
 }
